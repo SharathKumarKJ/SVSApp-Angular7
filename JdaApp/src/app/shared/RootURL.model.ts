3798c2d2ef927be0916e25f37df56f38ae2cb546
Sharath Kumar K J <kjsharath3@gmail.com>
2019-01-21 11:06:06 +0530

    changes

--- JdaApp/src/app/shared/RootURL.model.ts
+++ JdaApp/src/app/shared/RootURL.model.ts
@@ -1,6 +1,7 @@
 export class RootURL {
 
-    public static readonly URl = "https://svswebportal.azurewebsites.net";
+    public static readonly URl = "https://svswebapi.azurewebsites.net";
+    
     // public static readonly URl =  "http://localhost:84";
 
     // public static readonly URl =  "http://localhost:54937";
